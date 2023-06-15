805da98d98f2aa6b0b77361176b61d991e79b2b2
anmol2410 <125111205+anmol2410@users.noreply.github.com>
2023-06-15 08:01:15 +0000

    lecture 2 codehelp dsa

---
+++ type_cast.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char num = 75;
+    cout << num << endl;
+
+    unsigned int a = -5;
+    cout << a << endl;
+    // it convert into binary and taking 2's compliment it convert 1st bit to 1 and it will treat as postive value and give huge number
+
+    return 0;
+}
---
+++ variable_d_type.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a=12;
+    cout<<"a = "<<a<<endl;
+
+    char b='r';
+    cout<<"b = "<<b<<endl;
+
+    float f=5.0;
+    cout<<"f = "<<f<<endl;
+
+    double d=5.34;
+    cout<<"d = "<<d<<endl;
+
+    bool bl=true;
+    cout<<"bl = "<<bl<<endl;
+
+    cout<<"Size of bl = "<<sizeof(bl)<<endl;
+
+    return 0;
+}
